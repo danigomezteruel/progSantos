0816d935a542da0a0c47d5eb9afe16c3636b51db
Santaro13 <Ezrealsantos@gmail.com>
2026-01-06 15:52:46 +0100

    Ejercicio 5a y 5b (mates)

--- untitled/src/Practica5B/Practica5b.java
+++ untitled/src/Practica5B/Practica5b.java
@@ -2,12 +2,12 @@ package Practica5B;
 
 public class Practica5b {
     static void main(String[] args) {
-        Asignatura asignatura1=new Asignatura(1);
+        Asignatura Mates =new Asignatura(1);
         Asignatura asignatura3=new Asignatura(3);
         Asignatura asignatura2=new Asignatura(2);
 
 
-        Alumno alumno=new Alumno(asignatura1,asignatura2,asignatura3);
+        Alumno alumno=new Alumno(Mates,asignatura2,asignatura3);
 
 
        Profesor.ponerNotas(alumno);
@@ -15,7 +15,7 @@ public class Practica5b {
        Profesor.mediaNotas(alumno);
 
 
-        System.out.println(String.format("Nota Asignatura 1: %.2f", alumno.getAsignatura1().getNota()));
+        System.out.println(String.format("Nota Mates: %.2f", alumno.getAsignatura1().getNota()));
         System.out.println(String.format("Nota Asignatura 2: %.2f", alumno.getAsignatura2().getNota()));
         System.out.println(String.format("Nota Asignatura 3: %.2f", alumno.getAsignatura3().getNota()));
 
